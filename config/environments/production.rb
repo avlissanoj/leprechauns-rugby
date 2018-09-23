9fccb1cb91c4fc3d333f3c3c5915bc1a443af7ac
Jonas Alves <jonas.alves@live.com>
2018-09-23 13:10:46 -0300

    Test production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.asset_host = 'leprechauns-rugby.herokuapp.com'
+  config.action_mailer.asset_host = 'https://www.leprechauns-rugby.herokuapp.com'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

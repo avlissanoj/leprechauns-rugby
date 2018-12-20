6b85644c8b4120804e83208784fd537f3fed8491
Jonas Alves <jonas.alves@codus.com.br>
2018-12-20 09:26:42 -0200

    Added policies for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Você não está autorizado a fazer essa ação"
+    redirect_to(request.referrer || root_path)
+  end
 end

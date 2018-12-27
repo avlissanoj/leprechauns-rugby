2a007354c05c365e82fee304e9128e115752ab9e
Jonas <jonas.alves@live.com>
2018-12-26 23:50:02 -0200

    Added supporter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009135150) do
+ActiveRecord::Schema.define(version: 20181227002643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20181009135150) do
     t.datetime "last_update_at"
     t.datetime "last_age_registration_email_send_at"
     t.string "blood_type"
+    t.boolean "supporter", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
